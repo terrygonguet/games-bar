6dba7838dac3f1c70ff587ec3609f02b00e59b3c
terrygonguet <terry@gonguet.com>
2020-04-19 16:47:01 +0200

    Base svelte setup

---
+++ src/routes/about.svelte
@@ -0,0 +1,15 @@
+<script>
+	import { fade } from "svelte/transition";
+</script>
+
+<svelte:head>
+  <title>Games Bar - About</title>
+</svelte:head>
+
+<main
+  class="flex-center absolute w-full h-full"
+  in:fade={{ duration: 200, delay: 200 }}
+  out:fade={{ duration: 200 }}>
+  <p>About</p>
+  <a href="/">home</a>
+</main>
---
+++ src/routes/index.svelte
@@ -0,0 +1,15 @@
+<script>
+	import { fade } from "svelte/transition";
+</script>
+
+<svelte:head>
+  <title>Games Bar - Home</title>
+</svelte:head>
+
+<main
+  class="flex-center absolute w-full h-full"
+  in:fade={{ duration: 200, delay: 200 }}
+  out:fade={{ duration: 200 }}>
+  <p>YO</p>
+  <a href="about">about</a>
+</main>
